bca251660bcc84b7263138fac802908e067ef170
igta0222 <igor.tatarinov@netcracker.com>
2024-04-14 23:38:09 +0300

    DT

--- src/ch8/countdown2/countdown.go
+++ src/ch8/countdown2/countdown.go
@@ -15,22 +15,17 @@ import (
 //!+
 
 func main() {
-	// ...create abort channel...
-
-	//!-
-
-	//!+abort
 	abort := make(chan struct{})
+	i := 10
 	go func() {
 		os.Stdin.Read(make([]byte, 1)) // read a single byte
 		abort <- struct{}{}
 	}()
-	//!-abort
-
-	//!+
 	fmt.Println("Commencing countdown.  Press return to abort.")
 	select {
 	case <-time.After(10 * time.Second):
+		fmt.Println(i)
+		i--
 		// Do nothing.
 	case <-abort:
 		fmt.Println("Launch aborted!")
@@ -38,8 +33,6 @@ func main() {
 	}
 	launch()
 }
-
-//!-
 
 func launch() {
 	fmt.Println("Lift off!")
--- src/ch8/countdown3/countdown.go
+++ src/ch8/countdown3/countdown.go
@@ -18,24 +18,18 @@ import (
 //!+
 
 func main() {
-	// ...create abort channel...
-
-	//!-
-
 	abort := make(chan struct{})
 	go func() {
 		os.Stdin.Read(make([]byte, 1)) // read a single byte
 		abort <- struct{}{}
 	}()
 
-	//!+
 	fmt.Println("Commencing countdown.  Press return to abort.")
 	tick := time.Tick(1 * time.Second)
 	for countdown := 10; countdown > 0; countdown-- {
 		fmt.Println(countdown)
 		select {
 		case <-tick:
-			// Do nothing.
 		case <-abort:
 			fmt.Println("Launch aborted!")
 			return
@@ -43,9 +37,6 @@ func main() {
 	}
 	launch()
 }
-
-//!-
-
 func launch() {
 	fmt.Println("Lift off!")
 }
